f58f87e1f6d82588c2e0a5ae179c40f5f90f22df
Akshay kumar tyagi <79367523+akshaytyagi482@users.noreply.github.com>
2024-11-03 03:51:04 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
 import { DM_Sans as FontSans } from "next/font/google";
-
+import Head from 'next/head';
 import { Navbar, Footer } from "@/components/layout";
 
 import { ThemeProvider } from "@/components/providers/theme-provider";
